57793803e88c0f5382169b48f9a87134a7c6c966
Kamanzi Rama-Munroe <138566751+munroek@users.noreply.github.com>
2024-07-31 22:23:04 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_bMlFKWK7h', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'd0s0kio9m9ptd8j5qddtpo1h3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
